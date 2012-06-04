f8c2a9813f7d4c0519e7bb2f03e11a12b6cc777a
Robert Lis <robertmadman@gmail.com>
2012-06-04 22:29:50 +0100

    All changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 TwitterLike::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
+  root to: 'pages#home'
+  match '/contact', to: 'pages#contact'
+  match '/about', to: 'pages#about'
+  match '/help', to: 'pages#help'
   
-  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
