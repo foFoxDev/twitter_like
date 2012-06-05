aab07f76da3c5e3320411fa77b6f5004fd064251
Robert Lis <robertmadman@gmail.com>
2012-06-05 22:24:09 +0100

    Login Added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
   def new
     @title = " Sign up"
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the sample app! :)"
+      redirect_to @user
+    else
+      flash[:error] = "Ouch :(" 
+      render 'new' 
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TwitterLike::Application.routes.draw do
-  get "users/new"
+  
+  resources :users
 
   root to: 'pages#home'
   match '/contact', to: 'pages#contact'
